f59613705c3fe8b60eeac85d65f55cb4e7a40181
yeech <yeechingtann@gmail.com>
2024-02-10 00:34:10 +0800

    changed createRecipe form 'skill' part to have radio button

--- website/YCForms.py
+++ website/YCForms.py
@@ -1,8 +1,8 @@
-from wtforms import Form, StringField, TextAreaField, validators, FileField
+from wtforms import Form, StringField, TextAreaField, validators, FileField, RadioField
 
 class CreateRecipeForm(Form):
     title = StringField('Title', [validators.Length(min=1, max=150), validators.DataRequired()])
-    skill = StringField('Skill', [validators.Length(min=1, max=150), validators.DataRequired()])
+    skill = RadioField('Skill', choices=[('Easy', 'Easy'), ('Intermediate', 'Intermediate'), ('Hard', 'Hard')], validators=[validators.DataRequired()])
     time = StringField('Time', [validators.Length(min=1, max=150), validators.DataRequired()])
     cuisine = StringField('Cuisine', [validators.Length(min=1, max=150), validators.DataRequired()])
     instruction = TextAreaField('Instructions', [validators.DataRequired()])
